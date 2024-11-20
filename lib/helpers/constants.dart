2571506fcafd0eaa7fbe4d1b181f99f83d194015
念 <3209752489@qq.com>
2024-11-20 09:45:08 +0800

    添加constant常量类

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,26 @@
+﻿class Constants {
+  static const List<Map<String, String>> UrlFormats = [
+    {
+      'text': 'Slug',
+      'value': 'SLUG',
+    },
+    {
+      'text': 'Short ID',
+      'value': 'SHORT_ID',
+    },
+  ];
+
+  static const int DEFAULT_POST_PAGE_SIZE = 10;
+
+  static const int DEFAULT_ARCHIVES_PAGE_SIZE = 50;
+
+  static const int DEFAULT_FEED_COUNT = 10;
+
+  static const String DEFAULT_ARCHIVES_PATH = 'archives';
+
+  static const String DEFAULT_POST_PATH = 'post';
+
+  static const String DEFAULT_TAG_PATH = 'tag';
+
+  static const String DEFAULT_ROBOTS_TEXT = 'User-agent: *\nDisallow:';
+}
