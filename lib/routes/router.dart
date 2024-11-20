00eea19e4a6a221f5f23daa5349308823c76eca9
念 <3209752489@qq.com>
2024-11-20 09:45:47 +0800

    添加路由routers

---
+++ lib/routes/router.dart
@@ -0,0 +1,22 @@
+﻿import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppRouter {
+  static final List<GetPage<Widget>> routes = [
+    GetPage(
+      name: '/',
+      title: 'main',
+      page: () => const Home(),
+      children: [
+        GetPage(name: '/articles', title: 'articles', page: () => const Articles()),
+        GetPage(name: '/menu', title: 'menu', page: () => const Menu()),
+        GetPage(name: '/tags', title: 'tags', page: () => const Tags()),
+        GetPage(name: '/theme', title: 'theme', page: () => const Theme()),
+        GetPage(name: '/setting', title: 'setting', page: () => const Setting()),
+        GetPage(name: '/loading', title: 'loading', page: () => const Loading()),
+      ],
+      unknownRoute: GetPage(name: '*', page: () => const articles()),
+    ),
+  ];
+}
+
