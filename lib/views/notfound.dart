80ebe9fe34c08aa2e11e96959524e8324bb62695
念 <3209752489@qq.com>
2024-11-21 19:10:42 +0800

    将 HomePage 的内容移动柜到 HomeWidget 中，并添加了 NotFound 控件

---
+++ lib/views/notfound.dart
@@ -0,0 +1,11 @@
+﻿import 'package:flutter/material.dart';
+
+class NotfoundWidget extends StatelessWidget {
+  const NotfoundWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
