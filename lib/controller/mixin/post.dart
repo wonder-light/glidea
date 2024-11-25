5305dda632e3deebe4d25a0163c165202b2e8e7b
念 <3209752489@qq.com>
2024-11-26 03:41:38 +0800

    为 SiteController 添加 PostSite 混合类

---
+++ lib/controller/mixin/post.dart
@@ -0,0 +1,9 @@
+﻿import 'package:get/get.dart' show StateController;
+import 'package:glidea/models/application.dart';
+import 'package:glidea/models/post.dart';
+
+/// 混合 - 文章
+mixin PostSite on StateController<Application> {
+  /// 菜单
+  List<Post> get posts => state.posts;
+}
