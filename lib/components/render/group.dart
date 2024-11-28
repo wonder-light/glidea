3c7ecc57873e9f261452ffc4e83a7431e4bff9de
念 <3209752489@qq.com>
2024-11-28 16:35:33 +0800

    修改 GroupWidget 分组布局控件

--- lib/components/render/group.dart
+++ lib/components/render/group.dart
@@ -1,49 +1,63 @@
-﻿import 'dart:math';
-
-import 'package:flutter/material.dart';
-import 'package:glidea/models/render.dart';
+﻿import 'package:flutter/material.dart';
+import 'package:get/get.dart' show Trans;
 
 /// 分组布局控件
 class GroupWidget extends StatelessWidget {
   const GroupWidget({
     super.key,
-    required this.configs,
+    required this.children,
+    required this.groups,
     this.isTop = false,
+    this.isScrollable = false,
+    this.labelPadding,
+    this.tabAlignment = TabAlignment.start,
+    this.onTap,
   });
 
   /// 配置
-  final List<ConfigBase> configs;
+  final Set<String> groups;
+
+  /// 子节点
+  final List<Widget> children;
 
   /// 是否是最顶级的标签页
   final bool isTop;
 
+  /// 是否可以水平滚动此选项卡栏
+  final bool isScrollable;
+
+  /// 添加到每个制表标签上的填充
+  final EdgeInsets? labelPadding;
+
+  /// 指定选项卡内选项卡的对齐方式
+  final TabAlignment? tabAlignment;
+
+  /// 一个可选的回调在TabBar被点击时被调用
+  final ValueChanged<int>? onTap;
+
   @override
   Widget build(BuildContext context) {
-    Set<String> groups = configs.map((t) => t.group).toSet();
     // Tab
     Widget childWidget = TabBar.secondary(
-      isScrollable: !isTop,
+      isScrollable: isScrollable,
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 0),
+      labelPadding: labelPadding ?? EdgeInsets.symmetric(horizontal: isTop ? 24 : 6, vertical: isTop ? 0 : 24),
+      tabAlignment: tabAlignment,
+      onTap: onTap,
       tabs: [
         for (var item in groups)
           if (isTop)
-            Tab(text: item)
+            Tab(text: item.tr)
           else
             RotatedBox(
               quarterTurns: 1,
-              child: Tab(text: item),
+              child: Tab(text: item.tr),
             ),
       ],
     );
     // 内容
     Widget content = Expanded(
-      child: TabBarView(
-        children: [
-          for (var item in groups)
-            Container(
-              color: Colors.accents[Random().nextInt(10)],
-            ),
-        ],
-      ),
+      child: TabBarView(children: children),
     );
 
     if (!isTop) {
@@ -51,7 +65,6 @@ class GroupWidget extends StatelessWidget {
       childWidget = Row(
         children: [
           Column(
-            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               RotatedBox(quarterTurns: -1, child: childWidget),
             ],
