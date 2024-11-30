38a69964b4f65f5f312a24b3e70f01045a3cbfe5
念 <3209752489@qq.com>
2024-12-01 04:05:00 +0800

    添加构造函数

--- lib/models/render.dart
+++ lib/models/render.dart
@@ -6,17 +6,24 @@ import 'package:glidea/interfaces/types.dart';
 @jsonSerializable
 @Json(discriminatorProperty: 'type')
 abstract class ConfigBase<T> {
+  ConfigBase({
+    this.name = '',
+    this.label = '',
+    this.group = '',
+    this.note = '',
+  });
+
   /// 变量名称
   @JsonProperty()
-  String name = '';
+  String name;
 
   /// 显示的字段名称
   @JsonProperty()
-  String label = '';
+  String label;
 
   /// 字段的分组
   @JsonProperty()
-  String group = '';
+  String group;
 
   /// 字段的输入类型
   ///
@@ -26,7 +33,7 @@ abstract class ConfigBase<T> {
 
   /// 辅助文本, 展示在表单空间下面
   @JsonProperty()
-  String note = '';
+  String note;
 
   /// 字段默认值
   @JsonProperty()
@@ -39,45 +46,86 @@ abstract class ConfigBase<T> {
 /// 字符串值配置
 @jsonSerializable
 abstract class ConfigString extends ConfigBase<String> {
+  ConfigString({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    this.value = '',
+  });
+
   @override
   @JsonProperty()
-  String value = '';
+  String value;
 }
 
 /// 布尔值配置
 @jsonSerializable
 abstract class ConfigBool extends ConfigBase<bool> {
+  ConfigBool({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    this.value = false,
+  });
+
   @override
   @JsonProperty()
-  bool value = false;
+  bool value;
 }
 
 /// 整数值配置
 @jsonSerializable
-abstract class ConfigDouble extends ConfigBase<double> {
+abstract class ConfigInt extends ConfigBase<int> {
+  ConfigInt({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    this.value = 0,
+  });
+
   @override
   @JsonProperty()
-  double value = 0;
+  int value;
 }
 
 /// 数组映射值配置
 @jsonSerializable
 abstract class ConfigArrayMap extends ConfigBase<List<TJsonMap>> {
+  ConfigArrayMap({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    this.value = const [],
+  });
+
   /// 列表中的每一项数据都是对字段的值的记录
   @override
   @JsonProperty()
-  List<TJsonMap> value = [];
+  List<TJsonMap> value;
 }
 
 /// 富文本字段配置
 @jsonSerializable
 @Json(discriminatorValue: FieldType.textarea)
 class TextareaConfig extends ConfigString {
+  TextareaConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+    this.hint = '',
+  });
+
   /// 输入框中的提示文本
   ///
   /// [type] 为 'input'，'textarea' 时可用
   @JsonProperty()
-  String hint = '';
+  String hint;
 
   @override
   @JsonProperty()
@@ -88,6 +136,16 @@ class TextareaConfig extends ConfigString {
 @jsonSerializable
 @Json(discriminatorValue: FieldType.input)
 class InputConfig extends TextareaConfig {
+  InputConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+    super.hint,
+    this.card = InputCardType.none,
+  });
+
   /// card 配置
   ///
   /// 可选值：
@@ -96,7 +154,7 @@ class InputConfig extends TextareaConfig {
   ///     post:   提供文章数据卡片提供选择
   ///     none:   不显示卡片
   @JsonProperty()
-  InputCardType card = InputCardType.none;
+  InputCardType card;
 
   @override
   @JsonProperty()
@@ -107,19 +165,37 @@ class InputConfig extends TextareaConfig {
 @jsonSerializable
 @Json(discriminatorValue: FieldType.select)
 class SelectConfig extends ConfigString {
+  SelectConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+    this.options = const [],
+  });
+
   @override
   @JsonProperty()
   FieldType get type => FieldType.select;
 
   /// 选项列表
   @JsonProperty()
-  List<SelectOption> options = [];
+  List<SelectOption> options;
 }
 
 /// 单选按钮组配置
 @jsonSerializable
 @Json(discriminatorValue: FieldType.radio)
 class RadioConfig extends SelectConfig {
+  RadioConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+    super.options,
+  });
+
   @override
   @JsonProperty()
   FieldType get type => FieldType.radio;
@@ -129,6 +205,14 @@ class RadioConfig extends SelectConfig {
 @jsonSerializable
 @Json(discriminatorValue: FieldType.toggle)
 class ToggleConfig extends ConfigBool {
+  ToggleConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+  });
+
   @override
   @JsonProperty()
   FieldType get type => FieldType.toggle;
@@ -138,6 +222,14 @@ class ToggleConfig extends ConfigBool {
 @jsonSerializable
 @Json(discriminatorValue: FieldType.picture)
 class PictureConfig extends ConfigString {
+  PictureConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+  });
+
   @override
   @JsonProperty()
   FieldType get type => FieldType.picture;
@@ -147,34 +239,44 @@ class PictureConfig extends ConfigString {
 @jsonSerializable
 @Json(discriminatorValue: FieldType.array)
 class ArrayConfig extends ConfigArrayMap {
+  ArrayConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+    this.arrayItems = const [],
+  });
+
   @override
   @JsonProperty()
   FieldType get type => FieldType.array;
 
   /// 数组中的每一项数据记录的都是字段的定义
   @JsonProperty()
-  List<ConfigBase> arrayItems = [];
+  List<ConfigBase> arrayItems;
 }
 
 /// 滑块配置, 范围是 (0, ∞)
 @jsonSerializable
 @Json(discriminatorValue: FieldType.slider)
-class SliderConfig extends ConfigDouble {
+class SliderConfig extends ConfigInt {
+  SliderConfig({
+    super.name,
+    super.label,
+    super.group,
+    super.note,
+    super.value,
+    this.max = 100,
+  });
+
   @override
   @JsonProperty()
   FieldType get type => FieldType.slider;
 
   /// 滑动范围最大值, 必须大于 0
   @JsonProperty()
-  double max = 100;
-
-  /// 滑动时候的值是否是整数
-  ///
-  /// true: [value] 是转换成整数
-  ///
-  /// 默认是 false
-  @JsonProperty()
-  bool isInt = false;
+  int max;
 }
 
 /// 下拉列表中的选项
