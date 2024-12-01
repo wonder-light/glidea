e062d42e8de109ae1d69775e70d0f9637ccda00d
念 <3209752489@qq.com>
2024-12-01 08:10:06 +0800

    添加加载动画

--- lib/views/loading.dart
+++ lib/views/loading.dart
@@ -1,17 +1,264 @@
-﻿import 'package:flutter/material.dart';
+﻿import 'dart:math';
 
-class LoadingWidget extends StatefulWidget {
-  const LoadingWidget({super.key});
+import 'package:flutter/material.dart';
+import 'package:get/get.dart' show Trans;
+import 'package:glidea/helpers/constants.dart';
 
-  @override
-  State<LoadingWidget> createState() => _LoadingWidgetState();
-}
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({super.key, this.hint});
 
-class _LoadingWidgetState extends State<LoadingWidget> {
+  /// 提示文本
+  final String? hint;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          constraints: const BoxConstraints.expand(width: 80, height: 80),
+          child: const BallRotateChase(),
+        ),
+        const Padding(padding: kTopPadding16),
+        if (hint != null) Text(hint!.tr),
+      ],
+    );
   }
+}
+
+/// BallRotateChase.
+class BallRotateChase extends StatefulWidget {
+  const BallRotateChase({super.key});
+
+  @override
+  State<BallRotateChase> createState() => _BallRotateChaseState();
+}
+
+class _BallRotateChaseState extends State<BallRotateChase> with SingleTickerProviderStateMixin {
+  static const _durationInMills = 1500;
+  static const _ballNum = 5;
+
+  /// The index of shape in the widget.
+  final int index = 0;
+  late AnimationController _animationController;
+  final List<Animation<double>> _scaleAnimations = [];
+  final List<Animation<double>> _translateAnimations = [];
+
+  List<AnimationController> get animationControllers => [_animationController];
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: _durationInMills),
+    );
+    for (int i = 0; i < _ballNum; i++) {
+      final rate = i / 5;
+      final cubic = Cubic(0.5, 0.15 + rate, 0.25, 1.0);
+      _scaleAnimations.add(Tween(begin: 1 - rate, end: 0.2 + rate).animate(CurvedAnimation(parent: _animationController, curve: cubic)));
+      _translateAnimations.add(Tween(begin: 0.0, end: 2 * pi).animate(CurvedAnimation(parent: _animationController, curve: cubic)));
+
+      _animationController.repeat();
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (ctx, constraint) {
+      final circleSize = constraint.maxWidth / 5;
+
+      final deltaX = (constraint.maxWidth - circleSize) / 2;
+      final deltaY = (constraint.maxHeight - circleSize) / 2;
+
+      final widgets = List<Widget>.filled(_ballNum, Container());
+      for (int i = 0; i < _ballNum; i++) {
+        widgets[i] = Positioned.fromRect(
+          rect: Rect.fromLTWH(deltaX, deltaY, circleSize, circleSize),
+          child: AnimatedBuilder(
+            animation: _animationController,
+            builder: (_, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..translate(
+                    deltaX * sin(_translateAnimations[i].value),
+                    deltaY * -cos(_translateAnimations[i].value),
+                  ),
+
+                /// scale must in child, if upper would align topLeft.
+                child: ScaleTransition(
+                  scale: _scaleAnimations[i],
+                  child: child,
+                ),
+              );
+            },
+            /*child: IndicatorShapeWidget(
+              shape: Shape.circle,
+              index: i,
+            ),*/
+            child: Container(
+              constraints: const BoxConstraints(
+                minWidth: 36,
+                minHeight: 36,
+              ),
+              child: CustomPaint(
+                painter: _ShapePainter(
+                  Colors.accents[Random().nextInt(16)],
+                  Shape.circle,
+                  null,
+                  2,
+                  pathColor: null,
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+      return Stack(children: widgets);
+    });
+  }
+}
+
+/// Basic shape.
+enum Shape {
+  circle,
+  ringThirdFour,
+  rectangle,
+  ringTwoHalfVertical,
+  ring,
+  line,
+  triangle,
+  arc,
+  circleSemi,
+}
+
+class _ShapePainter extends CustomPainter {
+  final Color color;
+  final Shape shape;
+  final Paint _paint;
+  final double? data;
+  final double strokeWidth;
+  final Color? pathColor;
+
+  _ShapePainter(
+    this.color,
+    this.shape,
+    this.data,
+    this.strokeWidth, {
+    this.pathColor,
+  })  : _paint = Paint()..isAntiAlias = true,
+        super();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    switch (shape) {
+      case Shape.circle:
+        _paint
+          ..color = color
+          ..style = PaintingStyle.fill;
+        canvas.drawCircle(
+          Offset(size.width / 2, size.height / 2),
+          size.shortestSide / 2,
+          _paint,
+        );
+        break;
+      case Shape.ringThirdFour:
+        if (pathColor != null) {
+          _paint
+            ..color = pathColor!
+            ..strokeWidth = strokeWidth
+            ..style = PaintingStyle.stroke;
+          canvas.drawCircle(
+            Offset(size.width / 2, size.height / 2),
+            size.shortestSide / 2,
+            _paint,
+          );
+        }
+        _paint
+          ..color = color
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = strokeWidth;
+        canvas.drawArc(
+          Rect.fromCircle(
+            center: Offset(size.width / 2, size.height / 2),
+            radius: size.shortestSide / 2,
+          ),
+          -3 * pi / 4,
+          3 * pi / 2,
+          false,
+          _paint,
+        );
+        break;
+      case Shape.rectangle:
+        _paint
+          ..color = color
+          ..style = PaintingStyle.fill;
+        canvas.drawRect(Offset.zero & size, _paint);
+        break;
+      case Shape.ringTwoHalfVertical:
+        _paint
+          ..color = color
+          ..strokeWidth = strokeWidth
+          ..style = PaintingStyle.stroke;
+        final rect = Rect.fromLTWH(size.width / 4, size.height / 4, size.width / 2, size.height / 2);
+        canvas.drawArc(rect, -3 * pi / 4, pi / 2, false, _paint);
+        canvas.drawArc(rect, 3 * pi / 4, -pi / 2, false, _paint);
+        break;
+      case Shape.ring:
+        _paint
+          ..color = color
+          ..strokeWidth = strokeWidth
+          ..style = PaintingStyle.stroke;
+        canvas.drawCircle(Offset(size.width / 2, size.height / 2), size.shortestSide / 2, _paint);
+        break;
+      case Shape.line:
+        _paint
+          ..color = color
+          ..style = PaintingStyle.fill;
+        canvas.drawRRect(RRect.fromRectAndRadius(Rect.fromLTWH(0, 0, size.width, size.height), Radius.circular(size.shortestSide / 2)), _paint);
+        break;
+      case Shape.triangle:
+        final offsetY = size.height / 4;
+        _paint
+          ..color = color
+          ..style = PaintingStyle.fill;
+        Path path = Path()
+          ..moveTo(0, size.height - offsetY)
+          ..lineTo(size.width / 2, size.height / 2 - offsetY)
+          ..lineTo(size.width, size.height - offsetY)
+          ..close();
+        canvas.drawPath(path, _paint);
+        break;
+      case Shape.arc:
+        assert(data != null);
+        _paint
+          ..color = color
+          ..style = PaintingStyle.fill;
+        canvas.drawArc(Offset.zero & size, data!, pi * 2 - 2 * data!, true, _paint);
+        break;
+      case Shape.circleSemi:
+        _paint
+          ..color = color
+          ..style = PaintingStyle.fill;
+        canvas.drawArc(Offset.zero & size, -pi * 6, -2 * pi / 3, false, _paint);
+        break;
+    }
+  }
+
+  @override
+  bool shouldRepaint(_ShapePainter oldDelegate) =>
+      shape != oldDelegate.shape ||
+      color != oldDelegate.color ||
+      data != oldDelegate.data ||
+      strokeWidth != oldDelegate.strokeWidth ||
+      pathColor != oldDelegate.pathColor;
 }
