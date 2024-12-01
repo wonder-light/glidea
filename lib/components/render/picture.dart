7e4ea3b0e83526609dd720311262358f032e6461
念 <3209752489@qq.com>
2024-12-01 23:42:03 +0800

    保存时对图片进行压缩

--- lib/components/render/picture.dart
+++ lib/components/render/picture.dart
@@ -1,11 +1,17 @@
 ﻿import 'dart:io' show File;
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart' show Get, Inst, Obx;
+import 'package:get/get.dart' show Get, Inst, Obx, RxString, StringExtension;
 import 'package:glidea/controller/site.dart';
+import 'package:glidea/controller/theme.dart';
 import 'package:glidea/helpers/constants.dart';
+import 'package:glidea/helpers/events.dart';
 import 'package:glidea/helpers/fs.dart';
+import 'package:glidea/helpers/image.dart';
+import 'package:glidea/helpers/log.dart';
 import 'package:glidea/models/render.dart';
+import 'package:image/image.dart' as img show decodeImageFile;
+import 'package:image_picker/image_picker.dart' show ImagePicker, ImageSource;
 
 import 'base.dart';
 
@@ -20,14 +26,16 @@ class PictureWidget extends ConfigBaseWidget<PictureConfig> {
     super.onChanged,
   });
 
+
   @override
   Widget buildContent(BuildContext context, ThemeData theme) {
     var site = Get.find<SiteController>(tag: SiteController.tag);
+    var ctr = Get.find<ThemeController>(tag: site.themeCurrentTag);
+    final path = FS.joinR(site.state.appDir, ctr.pathDir, config.value.value).obs;
+    Log.i(path);
     return Obx(
       () => OutlinedButton(
-        onPressed: () {
-          // TODO: 打开图片选择器
-        },
+        onPressed: () => getImage(site, path),
         style: ButtonStyle(
           enableFeedback: true,
           tapTargetSize: MaterialTapTargetSize.shrinkWrap,
@@ -48,12 +56,36 @@ class PictureWidget extends ConfigBaseWidget<PictureConfig> {
             maxWidth: kImageWidth,
             maxHeight: kImageWidth * 2,
           ),
-          child: Image.file(
-            File(FS.joinR(site.state.appDir, config.value.value)),
+          child: Image(
+            image: FileImageExpansion(File(path.value)),
             fit: BoxFit.contain,
           ),
         ),
       ),
     );
+  }
+
+  void getImage(SiteController site, RxString path) async {
+    //实例化选择图片
+    final picker = ImagePicker();
+    //选择相册
+    final pickerImages = await picker.pickImage(source: ImageSource.gallery);
+    if (pickerImages == null || pickerImages.path.isEmpty) return;
+    // 保存的目标路径
+    var target = path.value;
+    // 选择的图片路径
+    path.value = FS.normalize(pickerImages.path);
+
+    /// 保存图片
+    Future<void> saveImage(arg) async {
+      // 保存并压缩
+      var image = await img.decodeImageFile(path.value);
+      await image?.compressImage(target);
+      //await pickerImages.saveTo(target);
+      // 恢复原有的路径
+      path.value = target;
+    }
+
+    site.once(themeSaveEvent, saveImage);
   }
 }
