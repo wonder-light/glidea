04c264a15135e52586d331495b69d008402de7ea
念 <3209752489@qq.com>
2024-12-03 03:26:11 +0800

    添加 FileImageExpansion.file 构造函数

--- lib/helpers/image.dart
+++ lib/helpers/image.dart
@@ -3,14 +3,22 @@
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img show Image, copyResize, encodeJpg, writeFile;
 
-///  扩展FileImage
+///  扩展 FileImage, 在图片变化时可以进行更新, 包括路径变化、大小变化等
 class FileImageExpansion extends FileImage {
   ///文件大小
   late final int fileSize;
 
-  FileImageExpansion(File file, {double scale = 1.0}) : super(file, scale: scale) {
+  /// 创建 [FileImageExpansion],
+  ///
+  /// 在图片变化时可以进行更新, 包括路径变化、大小变化等
+  FileImageExpansion(super.file, {super.scale}) {
     fileSize = file.lengthSync();
   }
+
+  /// 通过路径 [path] 创建 [FileImageExpansion],
+  ///
+  /// 在图片变化时可以进行更新, 包括路径变化、大小变化等
+  FileImageExpansion.file(String path, {double scale = 1.0}) : this(File(path), scale: scale);
 
   /// 除了比较路径和缩放比例以外，还要比较文件大小
   @override
@@ -24,10 +32,12 @@ class FileImageExpansion extends FileImage {
 }
 
 extension ImageSaveExt on img.Image {
-  /// 压缩图片
-  Future<bool> compressImage(String path, {int quality = 80, int scale = 100}) async {
-    var ratio = scale / 100;
-    var image = img.copyResize(this, width: (width * ratio).toInt(), height: (height * ratio).toInt());
+  /// 复制压缩图片到指定路径
+  ///
+  /// [quality] => [0, 100]
+  /// [scale] => (0, 1]
+  Future<bool> compressImage(String path, {int quality = 80, double scale = 1.0}) async {
+    var image = img.copyResize(this, width: (width * scale).toInt(), height: (height * scale).toInt());
     var lists = img.encodeJpg(image, quality: quality);
     return img.writeFile(path, lists);
   }
