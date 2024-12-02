94fc5555cc1f48c63931abbaf847b0f9ed8bfe87
念 <3209752489@qq.com>
2024-12-03 03:21:14 +0800

    提取布局使其看着更简洁

--- lib/components/render/base.dart
+++ lib/components/render/base.dart
@@ -8,18 +8,40 @@ abstract class ConfigBaseWidget<T extends ConfigBase> extends StatelessWidget {
   const ConfigBaseWidget({
     super.key,
     required this.config,
-    this.isTop = true,
-    this.ratio = 2.0,
-    this.labelPadding,
-    this.contentPadding,
-    this.helperPadding,
+    this.isVertical = true,
     this.onChanged,
   });
 
   /// true: 标题在顶部
   ///
   /// false: 标题在前面
-  final bool isTop;
+  final bool isVertical;
+
+  /// 下拉列表配置
+  ///
+  /// [config.label] 需要手动添加 [i18n] 翻译
+  final RxObject<T> config;
+
+  /// 当值发生变化时调用 - 主要用于 ArrayWidget 中接收值的变化
+  final ValueChanged<dynamic>? onChanged;
+}
+
+class ConfigLayoutWidget extends StatelessWidget {
+  const ConfigLayoutWidget({
+    super.key,
+    required this.config,
+    required this.child,
+    this.isVertical = true,
+    this.ratio = 2.0,
+    this.labelPadding,
+    this.contentPadding,
+    this.helperPadding,
+  });
+
+  /// true: 标题在顶部
+  ///
+  /// false: 标题在前面
+  final bool isVertical;
 
   /// [isTop] = [true] 时启用
   ///
@@ -29,7 +51,7 @@ abstract class ConfigBaseWidget<T extends ConfigBase> extends StatelessWidget {
   /// 下拉列表配置
   ///
   /// [config.label] 需要手动添加 [i18n] 翻译
-  final RxObject<T> config;
+  final ConfigBase config;
 
   /// 标签的边距
   ///
@@ -47,83 +69,99 @@ abstract class ConfigBaseWidget<T extends ConfigBase> extends StatelessWidget {
   ///     EdgeInsets.symmetric(horizontal: 12, vertical: 4)
   final EdgeInsetsGeometry? helperPadding;
 
-  /// 当值发生变化时调用 - 主要用于 ArrayWidget 中接收值的变化
-  final ValueChanged<dynamic>? onChanged;
+  /// 子节点控件
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    const base = 100;
-    var theme = Get.theme;
-    Widget childWidget = buildContent(context, theme);
-    // 标签
-    Widget label = Padding(
-      padding: labelPadding ?? (isTop ? kVerPadding8 : kRightPadding16),
-      child: Text(config.value.label.tr),
+    Widget childWidget = _buildContent(
+      child: child,
+      label: _buildLabel(),
+      note: _buildNote(),
     );
-    // 提示
-    Widget? note;
-    if (config.value.note.isNotEmpty) {
-      note = Padding(
-        padding: helperPadding ?? kVerPadding4,
-        child: Text(
-          config.value.note.tr,
-          style: theme.textTheme.bodySmall!.copyWith(
-            color: theme.colorScheme.outline,
-          ),
-        ),
-      );
-    }
-    // 垂直方向
-    if (isTop) {
-      childWidget = Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [label, childWidget, if (note != null) note],
-      );
-    } else {
-      // 比率
-      var value = (ratio * base).ceil();
-      // 给水平的方式添加包装
-      label = Flexible(flex: base, child: label);
-      // 将内容不能缩放的对齐在左边开始的位置
-      childWidget = Expanded(
-        flex: value,
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: childWidget,
-        ),
-      );
-      // 空状态
-      Widget empty = Flexible(flex: base, child: Container());
-      if (note != null) {
-        note = Flexible(flex: base + value, child: note);
-      }
-      // 控件的第一行
-      childWidget = Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [label, childWidget, empty],
-      );
-      if (note != null) {
-        // 提示文本的第二行
-        childWidget = Column(
-          children: [
-            childWidget,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [empty, note],
-            )
-          ],
-        );
-      }
-    }
 
     // 内容边距, 包裹所有控件的边距
-    if (contentPadding != null) childWidget = Padding(padding: contentPadding!, child: childWidget);
+    if (contentPadding != null) {
+      childWidget = Padding(padding: contentPadding!, child: childWidget);
+    }
 
     return childWidget;
   }
 
+  /// 构建标签
+  Widget? _buildLabel() {
+    // 如果是空的则返回 null
+    if (config.label.isEmpty) return null;
+    // 返回标签
+    return Padding(
+      padding: labelPadding ?? (isVertical ? kVerPadding8 : kVer8Hor12),
+      child: Text(config.label.tr),
+    );
+  }
+
+  /// 构建提示
+  Widget? _buildNote() {
+    // 如果是空的则返回 null
+    if (config.note.isEmpty) return null;
+    // 主题
+    var theme = Get.theme;
+    // 返回提示
+    return Padding(
+      padding: helperPadding ?? kVerPadding4,
+      child: Text(
+        config.note.tr,
+        style: theme.textTheme.bodySmall!.copyWith(
+          color: theme.colorScheme.outline,
+        ),
+      ),
+    );
+  }
+
   /// 构建内容
-  Widget buildContent(BuildContext context, ThemeData theme);
+  Widget _buildContent({required Widget child, Widget? label, Widget? note}) {
+    if (isVertical) {
+      // 垂直布局
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (label != null) label,
+          child,
+          if (note != null) note,
+        ],
+      );
+    }
+    // 基础比例
+    const base = 100;
+    // child 的比例
+    final value = (ratio * base).ceil();
+    // 加入提示
+    if (note != null) {
+      child = Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          child,
+          note,
+        ],
+      );
+    } else {
+      // 对齐
+      child = Align(
+        alignment: Alignment.centerLeft,
+        child: child,
+      );
+    }
+    // 水平布局
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(flex: base, child: label ?? Container()),
+        Expanded(flex: value, child: child),
+        Flexible(flex: base, child: Container()),
+      ],
+    );
+  }
 }
