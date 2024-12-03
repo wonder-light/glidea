a99f5ecaddcacca9e9d29629aa75a105235806b6
念 <3209752489@qq.com>
2024-12-03 09:11:47 +0800

    添加 DrawerDirection 抽屉动画的方向

--- lib/enum/enums.dart
+++ lib/enum/enums.dart
@@ -1,4 +1,5 @@
 ﻿import 'package:dart_json_mapper/dart_json_mapper.dart' show jsonSerializable;
+import 'package:flutter/material.dart' show Offset, Alignment;
 
 /// 部署的平台
 @jsonSerializable
@@ -58,4 +59,25 @@ enum InputCardType {
   card,
   post,
   none,
+}
+
+/// 抽屉动画的方向
+enum DrawerDirection {
+  /// 淡入进来
+  center(value: Offset.zero),
+  rightToLeft(value: Offset(1.0, 0.0)),
+  leftToRight(value: Offset(-1.0, 0.0)),
+  bottomToTop(value: Offset(0.0, 1.0)),
+  topToBottom(value: Offset(0.0, -1.0));
+
+  const DrawerDirection({required this.value});
+
+  /// Slide 动画开始的位置
+  final Offset value;
+
+  /// 是淡入
+  bool get isFade => value.dx == 0.0 && value.dy == 0.0;
+
+  /// 对齐
+  Alignment get toAlign => Alignment(value.dx, value.dy);
 }
