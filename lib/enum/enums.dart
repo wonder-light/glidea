992b7c775ce404bad14e05b8114f5559bcca1a3a
念 <3209752489@qq.com>
2024-12-04 04:15:57 +0800

    添加 Incident 枚举

--- lib/enum/enums.dart
+++ lib/enum/enums.dart
@@ -1,5 +1,6 @@
 ﻿import 'package:dart_json_mapper/dart_json_mapper.dart' show jsonSerializable;
 import 'package:flutter/material.dart' show Offset, Alignment;
+import 'package:get/get.dart' show Trans;
 
 /// 部署的平台
 @jsonSerializable
@@ -80,4 +81,33 @@ enum DrawerDirection {
 
   /// 对齐
   Alignment get toAlign => Alignment(value.dx, value.dy);
+}
+
+/// 事件
+enum Incident {
+  /// 成功
+  success(message: 'success'),
+
+  /// 同步遇到了错误，请查阅
+  syncError1(message: 'syncError1'),
+
+  /// 来寻找解决方案
+  syncError2(message: 'syncError2'),
+
+  /// 同步成功啦
+  syncSuccess(message: 'syncSuccess'),
+  deployError(message: 'deployError'),
+  deployError422(message: 'deployError422'),
+
+  /// 远程连接失败，请检查仓库、用户名和令牌设置
+  connectFailed(message: 'connectFailed');
+
+  // 构造
+  const Incident({required String message}) : _message = message;
+
+  /// 信息
+  final String _message;
+
+  /// 信息
+  String get message => _message.tr;
 }
