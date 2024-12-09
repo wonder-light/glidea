0d83ba9c03b0c1b9649c46d3467be2e07be0315b
念 <3209752489@qq.com>
2024-12-09 08:36:48 +0800

    添加 CustomTextController, 自定义控制器, 用于 TextFormField 的字体样式显示

---
+++ lib/views/post.dart
@@ -0,0 +1,225 @@
+﻿import 'package:flutter/material.dart';
+import 'package:get/get.dart' show Get, GetNavigationExt, Inst, Trans;
+import 'package:glidea/components/Common/page_action.dart';
+import 'package:glidea/controller/site.dart';
+import 'package:glidea/helpers/constants.dart';
+import 'package:glidea/helpers/get.dart';
+import 'package:glidea/helpers/json.dart';
+import 'package:glidea/interfaces/types.dart';
+import 'package:glidea/models/post.dart';
+import 'package:markdown_widget/markdown_widget.dart' show MarkdownWidget;
+import 'package:phosphor_flutter/phosphor_flutter.dart' show PhosphorIconsRegular;
+
+/// 自定义控制器, 用于文本编辑器的字体样式显示
+class CustomTextController extends TextEditingController {
+  CustomTextController() : super() {
+    _spanMaps = {
+      //标题 /(#+)(.*)/g
+      0: (reg: RegExp(r'(#+)(.*)'), match: _buildTitle),
+      // 链接 /!?\[[\s\S]*?\]\([\s\S]*?\)/g
+      1: (reg: RegExp(r'(!?\[)([\s\S]*?)(\]\([\s\S]*?\))'), match: _buildThreeSpan),
+      // 粗体
+      // 表示以一个 * 或者 _ 开头并结尾（\1表示规则和第一个集合相同）
+      // (\\*\\*|__)(.*?)(\\1)
+      2: (reg: RegExp(r'(\*\*|__)(.*?)(\1)'), match: (m) => _buildThreeSpan(m, style: _boldStyle, other: _titleStyle)),
+      // 斜体
+      // 表示以一个 * 或者 _ 开头并结尾（\1表示规则和第一个集合相同）
+      // (\\*|_)(.*?)(\\1)
+      3: (reg: RegExp(r'([*_])(.*?)(\1)'), match: (m) => _buildThreeSpan(m, style: _italicsStyle, other: _titleStyle)),
+      // 引用块
+      // \n(&gt;|\\>)(.*)
+      4: (reg: RegExp(r'(\n[&gt;|>])(.*)'), match: (m) => _buildTitle(m, index: 1)),
+      // 有序列表
+      // ^[\\s]*[0-9]+\\.(.*)
+      5: (reg: RegExp(r'(\s*[0-9]+\.)(\s*.*)'), match: (m) => _buildTitle(m, index: 1)),
+      // 无序列表
+      // ^[\\s]*[-\\*\\+] +(.*)
+      6: (reg: RegExp(r'(\s*[-*+]\s+)(.*)'), match: (m) => _buildTitle(m, index: 1)),
+      // 代码块
+      // ```([\\s\\S]*?)```[\\s]?
+      7: (reg: RegExp(r'(```)([\s\S]*?)(\1)'), match: (m) => _buildThreeSpan(m, style: _linkStyle, other: _titleStyle)),
+      // 内联代码块
+      // `{1,2}[^`](.*?)`{1,2}
+      8: (reg: RegExp(r'(`{1,2}[^`])(.*?)(`{1,2})'), match: (m) => _buildThreeSpan(m, style: _linkStyle, other: _titleStyle)),
+    };
+  }
+
+  TextStyle? _buildStyle;
+  TextStyle? _titleStyle;
+  TextStyle? _linkStyle;
+  TextStyle? _italicsStyle;
+  TextStyle? _boldStyle;
+  late final Map<int, TSpanMatch> _spanMaps;
+
+  @override
+  TextSpan buildTextSpan({required BuildContext context, TextStyle? style, required bool withComposing}) {
+    final bool composingRegionOutOfRange = !value.isComposingRangeValid || !withComposing;
+    if (!composingRegionOutOfRange) {
+      return super.buildTextSpan(context: context, style: style, withComposing: withComposing);
+    }
+    _buildStyle = style;
+    _titleStyle = style?.copyWith(color: Colors.cyan);
+    _linkStyle = style?.copyWith(color: Colors.black38);
+    _italicsStyle = style?.copyWith(fontStyle: FontStyle.italic);
+    _boldStyle = style?.copyWith(fontWeight: FontWeight.w700);
+    return TextSpan(children: _buildTextSpan(value: text));
+  }
+
+  /// 构建 [TextSpan]
+  List<TextSpan> _buildTextSpan({required String value, int index = 0}) {
+    if (value.isEmpty) return [];
+    // 没有规则, 之间返回
+    if (index >= _spanMaps.length) {
+      return [TextSpan(text: value, style: _buildStyle)];
+    }
+    assert(_spanMaps[index] != null, 'build TextSpan error, spanMaps[index] is null');
+    // 使用下一个规则
+    var spanMap = _spanMaps[index]!;
+    return value.splitMapWithSep(
+      pattern: spanMap.reg,
+      onMatch: spanMap.match,
+      onNonMatch: (str) => TextSpan(children: _buildTextSpan(value: str, index: index + 1)),
+    );
+  }
+
+  TextSpan _buildTitle(Match match, {int index = 0}) {
+    if (index <= 0) {
+      return TextSpan(text: match[index], style: _titleStyle);
+    } else {
+      return TextSpan(children: [
+        TextSpan(text: match[index], style: _titleStyle),
+        TextSpan(text: match[index + 1], style: _buildStyle),
+      ]);
+    }
+  }
+
+  TextSpan _buildThreeSpan(Match match, {TextStyle? style, TextStyle? other}) {
+    return TextSpan(children: [
+      TextSpan(text: match[1], style: other ?? _linkStyle),
+      TextSpan(text: match[2], style: style ?? _buildStyle),
+      TextSpan(text: match[3], style: other ?? _linkStyle),
+    ]);
+  }
+}
+
+/// post 文件编辑页面
+class PostView extends StatefulWidget {
+  const PostView({super.key});
+
+  @override
+  State<PostView> createState() => _PostViewState();
+}
+
+class _PostViewState extends State<PostView> {
+  /// 站点控制器
+  final site = Get.find<SiteController>(tag: SiteController.tag);
+
+  /// 获取的当前 post
+  late final RxObject<Post> currentPost;
+
+  /// 标题的字段控制器
+  final titleController = TextEditingController();
+
+  /// 标题的字段控制器
+  final contentController = CustomTextController();
+
+  @override
+  void initState() {
+    super.initState();
+    final fileName = Get.arguments as String;
+    currentPost = site.getPostOrDefault(fileName).obs;
+    titleController.text = currentPost.value.title;
+    contentController.text = currentPost.value.content;
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    contentController.dispose();
+    currentPost.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Get.theme;
+    // 字段装饰器
+    const decoration = InputDecoration(
+      isDense: true,
+      contentPadding: kVer8Hor12,
+      // 悬停时的背景色
+      hoverColor: Colors.transparent,
+      fillColor: Colors.transparent,
+      focusColor: Colors.transparent,
+      border: InputBorder.none,
+    );
+    // 标题
+    final titleWidget = FractionallySizedBox(
+      widthFactor: 0.7,
+      alignment: Alignment.center,
+      child: TextFormField(
+        controller: titleController,
+        decoration: decoration,
+      ),
+    );
+    // 内容
+    final content = Expanded(
+      child: FractionallySizedBox(
+        widthFactor: 0.7,
+        alignment: Alignment.center,
+        child: TextFormField(
+          controller: contentController,
+          maxLines: null,
+          decoration: decoration,
+          expands: true,
+        ),
+      ),
+    );
+    // 布局
+    return PageAction(
+      actions: [
+        IconButton(
+          onPressed: backToArticlePage,
+          icon: const Icon(PhosphorIconsRegular.arrowLeft),
+          tooltip: 'back'.tr,
+        ),
+        IconButton(
+          onPressed: saveAsDraft,
+          icon: const Icon(PhosphorIconsRegular.check),
+          tooltip: 'saveDraft'.tr,
+        ),
+        IconButton(
+          onPressed: savePost,
+          icon: Icon(PhosphorIconsRegular.check, color: theme.colorScheme.primary),
+          tooltip: 'save'.tr,
+        ),
+      ],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          titleWidget,
+          content,
+        ],
+      ),
+    );
+  }
+
+  /// 预览 post
+  Widget _buildPreview() {
+    return Expanded(
+      child: MarkdownWidget(data: currentPost.value.content),
+    );
+  }
+
+  /// 返回 article 页面
+  void backToArticlePage() {
+    Get.back();
+  }
+
+  /// 存草稿 post
+  void saveAsDraft() {}
+
+  /// 保存 post
+  void savePost() {}
+}
