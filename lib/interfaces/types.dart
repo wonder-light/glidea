b3042a5914f8e551c3755c6b14313e74cd55108a
念 <3209752489@qq.com>
2024-12-09 08:35:46 +0800

    添加 TSpanMatch 类型

--- lib/interfaces/types.dart
+++ lib/interfaces/types.dart
@@ -7,3 +7,6 @@ typedef TLinkData = ({String name, String link});
 typedef TJsonMap = Map<String, dynamic>;
 typedef TMap<T> = Map<String, T>;
 typedef TMaps<T> = Map<String, TMap<T>>;
+typedef TChangeCallback<T, F> = T Function(F value);
+typedef TChangeValue<T> = TChangeCallback<T, T>;
+typedef TSpanMatch = ({RegExp reg, TChangeCallback<TextSpan, Match> match});
