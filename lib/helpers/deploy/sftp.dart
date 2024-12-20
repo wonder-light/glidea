84f711f6571667cd2be9ed8589eec3baef7df29f
念 <3209752489@qq.com>
2024-12-20 09:33:45 +0800

    将 SftpDeploy 移动到 deploy/sftp.dart

---
+++ lib/helpers/deploy/sftp.dart
@@ -0,0 +1,12 @@
+﻿import 'deploy.dart';
+
+/// SFTP 部署
+class SftpDeploy extends Deploy {
+  SftpDeploy(super.site, {super.api = ''});
+
+  @override
+  Future<void> publish() async {
+    // TODO: implement publish
+    throw UnimplementedError();
+  }
+}
