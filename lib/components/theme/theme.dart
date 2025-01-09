66130a8d545216530530ef3515880ec52d3a088e
念 <3209752489@qq.com>
2025-01-09 22:51:03 +0800

    fix(ThemeView): 将主题与自定义主题提升到各自的组件中

---
+++ lib/components/theme/theme.dart
@@ -0,0 +1,120 @@
+﻿import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart' show BoolExtension, Get, Inst, Obx, Trans;
+import 'package:glidea/components/Common/loading.dart';
+import 'package:glidea/components/render/array.dart';
+import 'package:glidea/components/render/group.dart';
+import 'package:glidea/controller/site.dart';
+import 'package:glidea/helpers/constants.dart';
+import 'package:glidea/helpers/get.dart';
+import 'package:glidea/lang/base.dart';
+import 'package:glidea/models/render.dart';
+
+typedef TAsync = ValueSetter<AsyncCallback>;
+
+/// 主题控件
+class ThemeWidget extends StatefulWidget {
+  const ThemeWidget({super.key, required this.loadData});
+
+  /// 加载数据
+  final AsyncValueGetter<List<ConfigBase>>? loadData;
+
+  @override
+  State<ThemeWidget> createState() => ThemeWidgetState();
+}
+
+class ThemeWidgetState extends State<ThemeWidget> {
+  /// 站点控制器
+  final site = Get.find<SiteController>(tag: SiteController.tag);
+
+  /// 主题配置在加载中
+  final isLoading = false.obs;
+
+  /// 主题配置
+  final configs = <ConfigBase>[].obs;
+
+  /// 图片的限定范围
+  ///
+  ///     > 0  => 主题
+  ///     = 0  => 自定义主题
+  ///     < 0  => 其它
+  final int pictureScope = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      if (isLoading.value) {
+        return const Center(child: LoadingWidget());
+      }
+      return _buildContent(configs.value, isTop: false);
+    });
+  }
+
+  /// 从 [ConfigBase] 构建对应的控件
+  Widget _buildContent(Iterable<ConfigBase> items, {bool isTop = true}) {
+    return ListView.separated(
+      shrinkWrap: true,
+      padding: kHorPadding12 * 2 + kVerPadding16,
+      itemCount: items.length,
+      itemBuilder: (ctx, index) => ArrayWidget.create(config: items.elementAt(index), isVertical: isTop, scope: pictureScope),
+      separatorBuilder: (BuildContext context, int index) => const Padding(padding: kVerPadding8),
+    );
+  }
+
+  /// 加载主题
+  Future<void> loadData() async {
+    isLoading.value = true;
+    configs.value = await widget.loadData?.call() ?? [];
+    isLoading.value = false;
+  }
+}
+
+/// 自定义主题控件
+class ThemeCustomWidget extends ThemeWidget {
+  const ThemeCustomWidget({super.key, required super.loadData});
+
+  @override
+  State<ThemeWidget> createState() => ThemeCustomWidgetState();
+}
+
+class ThemeCustomWidgetState extends ThemeWidgetState {
+  @override
+  int get pictureScope => 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      if (isLoading.value) {
+        return const Center(child: LoadingWidget());
+      }
+      // 空的
+      if (configs.value.isEmpty) {
+        return Container(
+          alignment: Alignment.center,
+          padding: kAllPadding16,
+          child: Text(Tran.noCustomConfigTip.tr),
+        );
+      }
+      // 分组
+      Map<String, List<ConfigBase>> groups = {};
+      for (var t in configs.value) {
+        (groups[t.group] ??= []).add(t);
+      }
+      // 只有一个
+      if (groups.keys.length == 1) {
+        return _buildContent(groups.values.first, isTop: false);
+      }
+      // 分组布局
+      return GroupWidget(
+        groups: groups.keys.toList(),
+        itemBuilder: (ctx, index) => _buildContent(groups.values.elementAt(index)),
+      );
+    });
+  }
+}
--- lib/views/theme.dart
+++ lib/views/theme.dart
@@ -1,12 +1,10 @@
 ﻿import 'package:flutter/material.dart';
-import 'package:get/get.dart' show BoolExtension, Get, GetNavigationExt, Inst, Obx, Trans;
-import 'package:glidea/components/Common/loading.dart';
+import 'package:get/get.dart' show Get, GetNavigationExt, Inst, Trans;
 import 'package:glidea/components/Common/tip.dart';
-import 'package:glidea/components/render/array.dart';
 import 'package:glidea/components/render/group.dart';
+import 'package:glidea/components/theme/theme.dart';
 import 'package:glidea/controller/site.dart';
 import 'package:glidea/helpers/constants.dart';
-import 'package:glidea/helpers/events.dart';
 import 'package:glidea/helpers/get.dart';
 import 'package:glidea/lang/base.dart';
 import 'package:glidea/models/render.dart';
@@ -24,38 +22,17 @@ class _ThemeViewState extends State<ThemeView> {
   final site = Get.find<SiteController>(tag: SiteController.tag);
 
   /// 主题配置
-  final themeConfig = <ConfigBase>[].obs;
-
-  /// 主题在加载中
-  final themeLoading = false.obs;
+  var themeConfig = <ConfigBase>[];
 
   /// 自定义主题配置
-  final themeCustomConfig = <ConfigBase>[].obs;
+  var customConfig = <ConfigBase>[];
 
-  /// 自定主题在加载中
-  final customLoading = false.obs;
+  final themeKey = GlobalKey<ThemeWidgetState>();
+  final customKey = GlobalKey<ThemeCustomWidgetState>();
 
   @override
   void initState() {
     super.initState();
-    site.isThemeCustomPage = false;
-    if (Get.isPhone) {
-      site.isThemeCustomPage = Get.arguments == Tran.customConfig;
-      site.isThemeCustomPage! ? loadCustom() : loadTheme();
-    } else {
-      loadTheme();
-    }
-  }
-
-  @override
-  void dispose() {
-    themeConfig.dispose();
-    themeLoading.dispose();
-    customLoading.dispose();
-    themeCustomConfig.dispose();
-    site.off(themeSaveEvent);
-    site.isThemeCustomPage = null;
-    super.dispose();
   }
 
   @override
@@ -66,15 +43,10 @@ class _ThemeViewState extends State<ThemeView> {
     Widget childWidget = GroupWidget(
       isTop: true,
       groups: const [Tran.basicSetting, Tran.customConfig],
-      itemBuilder: (ctx, index) => index <= 0 ? buildThemeConfig() : buildCustomConfig(),
-      onTap: (index) {
-        site.isThemeCustomPage = index > 0;
-        if (index > 0 && themeCustomConfig.value.isEmpty) {
-          loadCustom();
-        }
-        if (index <= 0 && themeConfig.value.isEmpty) {
-          loadTheme();
-        }
+      itemBuilder: (ctx, index) {
+        final isThemePage = index <= 0;
+        final build = isThemePage ? ThemeWidget.new : ThemeCustomWidget.new;
+        return build(key: isThemePage ? themeKey : customKey, loadData: () => loadData(isThemePage));
       },
     );
     // PC 端和平板端
@@ -96,62 +68,12 @@ class _ThemeViewState extends State<ThemeView> {
   /// 构建手机端
   Widget buildPhone() {
     // arguments 参数来自 [package:glidea/views/setting.dart] 中的 [_SettingViewState.toRouter]
-    var arg = Get.arguments as String;
+    final arg = Get.arguments as String;
+    final isThemePage = Get.arguments != Tran.customConfig;
+    final build = isThemePage ? ThemeWidget.new : ThemeCustomWidget.new;
     return Scaffold(
       appBar: AppBar(title: Text(arg.tr), actions: getActionButton()),
-      body: site.isThemeCustomPage! ? buildCustomConfig() : buildThemeConfig(),
-    );
-  }
-
-  /// 构建主题配置页面的内容
-  Widget buildThemeConfig() {
-    return Obx(() {
-      if (themeLoading.value) {
-        return const Center(child: LoadingWidget());
-      }
-      return _buildContent(themeConfig.value, isTop: false);
-    });
-  }
-
-  /// 构建自定义主题配置页面的内容
-  Widget buildCustomConfig() {
-    return Obx(() {
-      if (customLoading.value) {
-        return const Center(child: LoadingWidget());
-      }
-      // 空的
-      if (themeCustomConfig.value.isEmpty) {
-        return Container(
-          alignment: Alignment.center,
-          padding: kAllPadding16,
-          child: Text(Tran.noCustomConfigTip.tr),
-        );
-      }
-      // 分组
-      Map<String, List<ConfigBase>> groups = {};
-      for (var t in themeCustomConfig.value) {
-        (groups[t.group] ??= []).add(t);
-      }
-      // 只有一个
-      if (groups.keys.length == 1) {
-        return _buildContent(groups.values.first, isTop: false);
-      }
-      // 分组布局
-      return GroupWidget(
-        groups: groups.keys.toList(),
-        itemBuilder: (ctx, index) => _buildContent(groups.values.elementAt(index)),
-      );
-    });
-  }
-
-  /// 从 [ConfigBase] 构建对应的控件
-  Widget _buildContent(Iterable<ConfigBase> items, {bool isTop = true}) {
-    return ListView.separated(
-      shrinkWrap: true,
-      padding: kHorPadding12 * 2 + kVerPadding16,
-      itemCount: items.length,
-      itemBuilder: (ctx, index) => ArrayWidget.create(config: items.elementAt(index), isVertical: isTop),
-      separatorBuilder: (BuildContext context, int index) => const Padding(padding: kVerPadding8),
+      body: build(key: isThemePage ? themeKey : customKey, loadData: () => loadData(isThemePage)),
     );
   }
 
@@ -176,7 +98,7 @@ class _ThemeViewState extends State<ThemeView> {
       TipWidget.down(
         message: Tran.reset.tr,
         child: IconButton(
-          onPressed: site.isThemeCustomPage! ? loadCustom : loadTheme,
+          onPressed: resetConfig,
           icon: const Icon(PhosphorIconsRegular.clockCounterClockwise),
         ),
       ),
@@ -190,31 +112,33 @@ class _ThemeViewState extends State<ThemeView> {
     ];
   }
 
-  /// 加载主题
-  Future<void> loadTheme() async {
-    themeLoading.value = true;
-    themeConfig.value = site.getThemeWidgetConfig();
-    themeLoading.value = false;
-  }
-
-  /// 加载自定义主题
-  Future<void> loadCustom() async {
-    customLoading.value = true;
-    themeCustomConfig.value = site.getThemeCustomWidgetConfig();
-    customLoading.value = false;
+  /// 加载数据
+  Future<List<ConfigBase>> loadData(bool isThemePage) async {
+    if (isThemePage) {
+      if (themeConfig.isEmpty) {
+        themeConfig = site.getThemeWidgetConfig();
+      }
+      return themeConfig;
+    } else {
+      if (customConfig.isEmpty) {
+        customConfig = site.getThemeCustomWidgetConfig();
+      }
+      return customConfig;
+    }
   }
 
   /// 重置配置
-  void resetConfig() {
-    loadTheme();
-    loadCustom();
+  void resetConfig() async {
+    themeConfig.clear();
+    customConfig.clear();
+    await themeKey.currentState?.loadData();
+    await customKey.currentState?.loadData();
   }
 
   /// 保存配置
   void saveConfig() async {
     // 保存前需要发出保存事件以便于图片进行保存
-    await site.emit(themeSaveEvent);
-    final value = await site.updateThemeConfig(themes: themeConfig.value, customs: themeCustomConfig.value);
+    final value = await site.updateThemeConfig(themes: themeConfig, customs: customConfig);
     value ? Get.success(Tran.themeConfigSaved) : Get.error(Tran.saveError);
     resetConfig();
   }
